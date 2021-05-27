7e941a30bd224ed742f59f7819276284ba8b9a96
dsab123 <dsabbaghumd@gmail.com>
2021-05-27 07:03:44 -0400

    pretty popup styling woot

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,41 @@
 import React from "react";
 import { signIn, signOut, useSession } from 'next-auth/client';
+import dynamic from "next/dynamic";
+
+const ReactTooltip = dynamic(() => import('react-tooltip'), {
+    ssr: false
+  });
 
 export function Navbar() {
     const [ session, loading ] = useSession();
   
     return <>
         <div className="text-left flex flex-row justify-between">
-            <button 
-                className="text-gray-400 font-bold py-1 px-2 cursor-pointer hover:text-white"
-                onClick={() => console.log('I\'m a person')}>
-                    Create Collection
-            </button>
-
+            <div 
+                data-tip="Coming Soon!" data-event="click" data-for="createCollection"
+                className="text-gray-400 font-bold py-1 px-2 hover:text-white cursor-pointer rounded">
+                Create Quotes Collection 📃
+            </div>
+            <ReactTooltip 
+                globalEventOff="mouseover"
+                id='createCollection'
+                effect='solid' 
+                place='right'
+                delayHide={500}
+                arrowColor='transparent'
+                backgroundColor='#7F221E'
+                overridePosition={ (
+                    { left, top },
+                    currentEvent, currentTarget, node) => {
+                  left =(document.documentElement.clientWidth - node.clientWidth) / 2;
+                  return { top, left }
+                } }
+            >
+                <p className="text-white text-2xl">
+                    Coming Soon!
+                </p>
+            </ReactTooltip>
+            
             {!session && <>
                 <button 
                     className="text-white font-bold py-1 px-2 cursor-pointer hover:underline"
